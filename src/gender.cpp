96f39824802162e3ecdad53316cd54198e558386
Yuri Pirola <yuri.pirola@gmail.com>
2010-10-05 12:01:55 +0200

    ZRHC*, added representation of individuals' sex.

---
+++ src/gender.cpp
@@ -0,0 +1,6 @@
+#include "gender.hpp"
+
+const gender_t gender_t::MALE(gender_t::_MALE),
+  gender_t::FEMALE(gender_t::_FEMALE),
+  gender_t::UNSPEC(gender_t::_UNSPEC);
+
---
+++ src/gender.hpp
@@ -0,0 +1,66 @@
+/**
+ *
+ * gender.hpp
+ *
+ * Simple enum-style class to represent genders.
+ *
+ **/
+#ifndef __GENDER_HPP__
+#define __GENDER_HPP__
+
+class gender_t {
+
+private:
+  enum gender_value {
+	 _UNSPEC= 0,
+	 _MALE= 1,
+	 _FEMALE= 2
+  } _g;
+
+  explicit gender_t(gender_value g)
+		:_g(g)
+  {}
+
+public:
+  static const gender_t UNSPEC, MALE, FEMALE;
+
+  gender_t(const gender_t& base)
+		:_g(base._g)
+  {}
+
+  bool is_male() {
+	 return _g == _MALE;
+  }
+
+  bool is_female() {
+	 return _g == _FEMALE;
+  }
+
+  bool is_not_specified() {
+	 return _g == _UNSPEC;
+  }
+
+  bool is_not_male() {
+	 return _g != _MALE;
+  }
+
+  bool is_not_female() {
+	 return _g != _FEMALE;
+  }
+
+  bool is_specified() {
+	 return _g != _UNSPEC;
+  }
+
+  friend bool operator==(const gender_t& g1, const gender_t& g2) {
+	 return g1._g==g2._g;
+  }
+
+  friend bool operator!=(const gender_t& g1, const gender_t& g2) {
+	 return g1._g!=g2._g;
+  }
+
+};
+
+
+#endif // __GENDER_HPP__
