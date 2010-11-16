91e8861ce0c3b0a8da49bd6ef1519f911fc694f8
Yuri Pirola <yuri.pirola@gmail.com>
2010-11-16 18:05:15 +0100

    ZRHC*, structure to store a SAT instance derived from a single-family pedigree.

---
+++ src/lib/pedcnf.cpp
@@ -0,0 +1,117 @@
+/**
+ *
+ *                              ZRHC-*
+ * Zero-Recombinant Haplotype Configuration with missing genotypes
+ *
+ * Copyright (C) 2010  Yuri Pirola <yuri.pirola(-at-)gmail.com>
+ *
+ * Distributed under the terms of the GNU General Public License (GPL)
+ *
+ *
+ * This file is part of ZRHC-* (ZRHCstar).
+ *
+ * ZRHC-* is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ZRHC-* is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ZRHC-*.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+/**
+ *
+ * pedcnf.cpp
+ *
+ * Structures to represent SAT instances derived from pedigrees.
+ *
+ **/
+
+#include "pedcnf.hpp"
+#include <iomanip>
+
+const ped_var_kind ped_var_kind::H(0);
+const ped_var_kind ped_var_kind::W(1);
+const ped_var_kind ped_var_kind::S(2);
+const ped_var_kind ped_var_kind::DUMMY(3);
+const int ped_var_kind::int_values[]={0, 1, 2, 3};
+const std::string ped_var_kind::str_values[]={"h", "w", "s", "dummy"};
+const ped_var_kind ped_var_kind::enum_values[]={H, W, S, DUMMY};
+
+std::ostream&
+operator<<(std::ostream& out, const pedcnf_t::pedvar_t& var) {
+  out << var.get<0>() << "_" << var.get<1>() << "_" << var.get<2>();
+  return out;
+}
+
+
+inline int
+pedcnf_t::get_var(varmap_t& map,
+						const ped_var_kind& var_kind,
+						const size_t i1, const size_t i2) {
+  varmap_t::iterator it= map.find(boost::make_tuple(i1, i2));
+  if (it == map.end()) {
+	 _vars.push_back(boost::make_tuple(var_kind, i1, i2));
+	 std::pair< varmap_t::iterator, bool> ret= map.insert(std::make_pair(boost::make_tuple(i1, i2), _vars.size()));
+	 MY_ASSERT(ret.second);
+	 it= ret.first;
+  }
+  return it->second;
+};
+
+int
+pedcnf_t::get_h(const size_t i, const size_t l) {
+  return get_var(_h, ped_var_kind::H, i, l);
+};
+
+int
+pedcnf_t::get_w(const size_t i, const size_t l) {
+  return get_var(_w, ped_var_kind::W, i, l);
+};
+
+int
+pedcnf_t::get_s(const size_t p, const size_t i) {
+  return get_var(_s, ped_var_kind::S, p, i);
+};
+
+int
+pedcnf_t::get_dummy(const int v1, const int v2) {
+  return get_var(_dummy, ped_var_kind::DUMMY, v1, v2);
+};
+
+std::ostream&
+pedcnf_t::clauses_to_dimacs_format(std::ostream& out) const {
+  out << "c SAT instance" << std::endl;
+  out << "c" << std::endl;
+  size_t i= 1;
+  for (pedcnf_t::varvec_t::const_iterator it= _vars.begin();
+		 it != _vars.end();
+		 ++it, ++i) {
+	 out << "c v " << std::setw(5) << i << " " << *it << std::endl;
+  }
+  out << "c" << std::endl;
+  out << "p cnf " << _vars.size() << " " << _clauses.size() << std::endl;
+  for (clauses_t::const_iterator it= _clauses.begin();
+		 it != _clauses.end();
+		 ++it) {
+	 out << *it << std::endl;
+  }
+  return out;
+};
+
+
+std::ostream&
+operator<<(std::ostream& out, const pedcnf_t::clause_t& clause) {
+  for (pedcnf_t::clause_t::const_iterator it= clause.begin();
+		 it != clause.end();
+		 ++it) {
+	 out << std::setw(6) << *it << " ";
+  }
+  out << "     0";
+  return out;
+};
