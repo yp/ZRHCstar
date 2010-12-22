e1ec685d5f77b06b1e285808ede09dddf7ab8a39
Yuri Pirola <yuri.pirola@gmail.com>
2010-12-22 19:07:33 +0100

    ZRHC*, fixed wrong switch for reporting the system time.

--- scripts/execute-ZRHC.sh
+++ scripts/execute-ZRHC.sh
@@ -49,5 +49,5 @@ LANG=C
 
 for pedigree in ${pedigrees}; do
     echo "`date`  --  Executing on ${pedigree}"
-    nice time -f "%U %s %E %x %M %C" -o ${pedigree/#gen-/time-} ${ZRHC_exe} -3 -p ${pedigree} -h ${pedigree/#gen-/hap-} -c "${SAT_cmd}"
+    nice time -f "%U %S %E %x %M %C" -o ${pedigree/#gen-/time-} ${ZRHC_exe} -3 -p ${pedigree} -h ${pedigree/#gen-/hap-} -c "${SAT_cmd}"
 done
